787034f1f88acea85b0479c4b29e733437b7c071
unknown <edwardachike@gmail.com>
2022-05-09 11:07:16 -0700

    toggle hambuger menu

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById("hambuger")
+const hhh = document.getElementById("nav-list")
+
+hamburger.addEventListener("click", function(event){
+    hhh.classList.toggle('vvv')
+    console.log(hhh) 
+
+})
