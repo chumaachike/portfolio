fbdd126ba2bbe3494872069aca2d0819ed296f1d
unknown <edwardachike@gmail.com>
2022-05-10 01:15:15 -0700

    finished mobile menu page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-const hamburger = document.getElementById("hambuger")
-const hhh = document.getElementById("nav-list")
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const branding = document.querySelector(".nav-branding");
+const xxx = document.querySelector(".opa");
 
-hamburger.addEventListener("click", function(event){
-    hhh.classList.toggle('vvv')
-    console.log(hhh) 
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    branding.classList.toggle("opacity");
+    xxx.classList.toggle("opacity");
+    console.log(branding)
+    
+
 
 })
+
+document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click", ()=>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    branding.classList.remove("opacity");
+    xxx.classList.remove("opacity");
+}))
+
