0fb9302099a60f19c83e3bf212b125ab8865bd8a
unknown <edwardachike@gmail.com>
2022-05-10 02:52:22 -0700

    solving linter issues -2

--- js/script.js
+++ js/script.js
@@ -2,24 +2,15 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const branding = document.querySelector('.nav-branding');
 const xxx = document.querySelector('.opa');
-const body = document.querySelector("body");
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   branding.classList.toggle('opacity');
   xxx.classList.toggle('opacity');
-  if (!hamburger.classList.contains('active')) {
-    // Disable scroll
-    body.style.overflow = 'hidden';
-  } else {
-    // Enable scroll
-    body.style.overflow = 'auto';
-  }
 });
 
-document.querySelectorAll('.nav-link').forEach((n) =>
-  n.addEventListener('click', () => {
+document.querySelectorAll('.nav-link').forEach((n) =>n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
     branding.classList.remove('opacity');
