1c5167b6146da9fcd6711601ab67be7fcfde3da2
unknown <edwardachike@gmail.com>
2022-05-10 01:27:22 -0700

    setup linters for JS

--- js/script.js
+++ js/script.js
@@ -8,11 +8,7 @@ hamburger.addEventListener("click", ()=>{
     navMenu.classList.toggle("active");
     branding.classList.toggle("opacity");
     xxx.classList.toggle("opacity");
-    console.log(branding)
-    
-
-
-})
+    })
 
 document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click", ()=>{
     hamburger.classList.remove("active");
