13b28f706f041ececf77b512ab20cdbead507f7a
unknown <edwardachike@gmail.com>
2022-05-10 02:42:28 -0700

    solving linter issues -1

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const branding = document.querySelector(".nav-branding");
-const xxx = document.querySelector(".opa");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const branding = document.querySelector('.nav-branding');
+const xxx = document.querySelector('.opa');
+const body = document.querySelector("body");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-  branding.classList.toggle("opacity");
-  xxx.classList.toggle("opacity");
-  if (!hamburger.classList.contains("active")) {
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  branding.classList.toggle('opacity');
+  xxx.classList.toggle('opacity');
+  if (!hamburger.classList.contains('active')) {
     // Disable scroll
-    body.style.overflow = "hidden";
+    body.style.overflow = 'hidden';
   } else {
     // Enable scroll
-    body.style.overflow = "auto";
+    body.style.overflow = 'auto';
   }
 });
 
-document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-    branding.classList.remove("opacity");
-    xxx.classList.remove("opacity");
-    body.style.overflow = "auto";
-  })
-);
+document.querySelectorAll('.nav-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+    branding.classList.remove('opacity');
+    xxx.classList.remove('opacity');
+    body.style.overflow = 'auto';
+  }));
