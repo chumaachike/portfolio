289d89375daed046b57e1c70122c88c3d5e38064
unknown <edwardachike@gmail.com>
2022-05-12 23:28:14 -0700

    correcting linter errors

--- js/script.js
+++ js/script.js
@@ -229,11 +229,11 @@ closeBtn.addEventListener('click', () => {
   closeModal(modal);
 });
 
-document.getElementById("contact-form").addEventListener("submit", (event) => {
-  const email = document.getElementById("contact-me-email").value;
+document.getElementById('contact-form').addEventListener('submit', (event) => {
+  const email = document.getElementById('contact-me-email').value;
   if (email !== email.toLowerCase()) {
     event.preventDefault();
-   const emailError = document.getElementById('email-error')
-   emailError.classList.add('active')
-  } 
+    const emailError = document.getElementById('email-error')
+    emailError.classList.add('active');
+  }
 });
